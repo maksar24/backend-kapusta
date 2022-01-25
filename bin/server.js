16ba343f7a8e792776bb1bf0697ce5046b796e39
Alexandr Pryadchenko <56647239+eternalvision@users.noreply.github.com>
2022-01-25 22:31:29 +0200

    reloaded files

--- bin/server.js
+++ bin/server.js
@@ -1,16 +1,17 @@
+const app = require("../app");
 const mongoose = require("mongoose");
 
-const app = require("../app");
-
-const { DB_HOST, PORT = 3000 } = process.env;
+const { PORT, DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => {
-    console.log("Database connection successful");
-    app.listen(PORT);
-  })
-  .catch((error) => {
-    console.log(error.message);
+  .then(() => console.log("Database connection successful"))
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.log(err.message);
     process.exit(1);
   });
